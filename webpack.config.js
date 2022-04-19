820b270f2048de895c4f7bd75c80ea14ac9b9c3d
Przemysław Krasiński <przemek.krasinski@gmail.com>
2022-04-19 15:56:44 -0500

    feat(webpack): Add webpack dev server configuration

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   mode: 'development',
+   entry: {
+     index: './src/index.js'
+   },
+   devtool: 'inline-source-map',
+   devServer: {
+     static: './dist',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'RekinySukcesu.pl Zadanie rekrutacyjne programista',
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+    publicPath: '/',
+   },
+ };
